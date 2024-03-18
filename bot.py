c4d339231e2d69ef742520fe700a7e57ad24fe8a
Arsenio <arseniy.kolomeytsev@gmail.com>
2024-03-18 17:17:54 +0300

    rework - made commands

--- bot.py
+++ bot.py
@@ -9,41 +9,24 @@ engine = create_engine('sqlite:///database.db')
 engine.connect()
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    user = User(message.chat.id)
-    ACTIVE_CHATS.append(user)
-    keyboard = InlineKeyboardMarkup(resize_keyboard=True)
-    button = InlineKeyboardButton(text="Начать работу", callback_data='main_menu')
-    keyboard.add(button)
-    bot.send_message(message.chat.id, reply_markup = keyboard)
-
-@bot.callback_query_handler(func=lambda call: call.data == 'main_menu')
-def main_menu(call):
-    keyboard = InlineKeyboardMarkup(resize_keyboard=True)
-    button1 = InlineKeyboardButton(text="Просмотреть старые графики", callback_data='show_graphics')
-    button2 = InlineKeyboardButton(text="Построить график", callback_data='plot')
-    keyboard.add(button1, button2)
-    bot.send_message(call.message.chat.id, reply_markup=keyboard)
-
-@bot.callback_query_handler(func=lambda call: call.data == 'show_graphics')
-def main_menu(call):
-    keyboard = InlineKeyboardMarkup(resize_keyboard=True)
+@bot.message_handler(commands=['show_graphics'])
+def main_menu(message):
+    keyboard = InlineKeyboardMarkup()
     button1 = InlineKeyboardButton(text="Вернуться на главную", callback_data='main_menu')
-    button2 = InlineKeyboardButton(text="Построить график", callback_data='next_page')
-    button3 = InlineKeyboardButton(text="Построить график", callback_data='prev_page')
+    button2 = InlineKeyboardButton(text="<", callback_data='next_page')
+    button3 = InlineKeyboardButton(text=">", callback_data='prev_page')
     keyboard.add(button1, button2, button3)
-    bot.send_message(call.message.chat.id, text='Графики', reply_markup=keyboard)
+    bot.send_message(message.chat.id, text='Графики', reply_markup=keyboard)
 
-@bot.callback_query_handler(func=lambda call: call.data == 'plot')
-def start_plotting(call):
-    bot.register_next_step_handler(bot.send_message(call.message.chat.id, text='Отправьте файл с данными'), get_data)
+@bot.message_handler(commands=['new_plot'])
+def start_plotting(message):
+    bot.register_next_step_handler(bot.send_message(message.chat.id, text='Отправьте файл с данными'), get_data)
 
 def get_data(message):
     '''file_info = bot.get_file(message.document.file_id)
     downloaded_file = bot.download_file(file_info.file_path)'''
 
-    keyboard = InlineKeyboardMarkup(resize_keyboard=True)
+    keyboard = InlineKeyboardMarkup()
     button1 = InlineKeyboardButton(text="Тип графика 1", callback_data='plot_type_1')
     button2 = InlineKeyboardButton(text="Тип графика 2", callback_data='plot_type_2')
     button3 = InlineKeyboardButton(text="Тип графика 2", callback_data='plot_type_3')
@@ -52,7 +35,7 @@ def get_data(message):
 
 @bot.callback_query_handler(func=lambda call: call.data == 'plot_type_1')
 def plot_type_1(call):
-    keyboard = InlineKeyboardMarkup(resize_keyboard=True)
+    keyboard = InlineKeyboardMarkup()
     button1 = InlineKeyboardButton(text="Сохранить график", callback_data='save_plot')
     button2 = InlineKeyboardButton(text="Скачать график", callback_data='download_plot')
     keyboard.add(button1, button2)
@@ -60,7 +43,7 @@ def plot_type_1(call):
 
 @bot.callback_query_handler(func=lambda call: call.data == 'plot_type_2')
 def plot_type_2(call):
-    keyboard = InlineKeyboardMarkup(resize_keyboard=True)
+    keyboard = InlineKeyboardMarkup()
     button1 = InlineKeyboardButton(text="Сохранить график", callback_data='save_plot')
     button2 = InlineKeyboardButton(text="Скачать график", callback_data='download_plot')
     keyboard.add(button1, button2)
@@ -68,7 +51,7 @@ def plot_type_2(call):
 
 @bot.callback_query_handler(func=lambda call: call.data == 'plot_type_3')
 def plot_type_3(call):
-    keyboard = InlineKeyboardMarkup(resize_keyboard=True)
+    keyboard = InlineKeyboardMarkup()
     button1 = InlineKeyboardButton(text="Сохранить график", callback_data='save_plot')
     button2 = InlineKeyboardButton(text="Скачать график", callback_data='download_plot')
     keyboard.add(button1, button2)
